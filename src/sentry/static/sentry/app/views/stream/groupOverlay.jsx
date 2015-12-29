8244e8eadb77e595611c24506a350fcb017cb727
bold <bold@oluul.com>
2015-12-29 19:38:29 +0800

    update

--- src/sentry/static/sentry/app/views/stream/groupOverlay.jsx
+++ src/sentry/static/sentry/app/views/stream/groupOverlay.jsx
@@ -114,39 +114,42 @@ const GroupEventDetails = React.createClass({
     let group = this.state.group;
     let evt = this.state.event;
     let params = this.props.params;
-
+    console.log(this.state);
     return (
       <div className="group-overlay">
         <div className="row event">
-          <div className="col-md-12">
-          {evt &&
-            <GroupEventToolbar group={group} event={evt} orgId={params.orgId} projectId={params.projectId} />
-          }
-          {group.status === 'muted' &&
-          <MutedBox statusDetails={group.statusDetails} />
-          }
-          {group.status === 'resolved' && group.statusDetails.inNextRelease &&
-          <div className="alert alert-info alert-block">
-            <span>{t(`This issue has been marked as being resolved in the next
-                        release. Until then, you will not get notified about new
-                        occurrences.`)}</span>
+          <div className="col-md-9">
+            {evt &&
+              <GroupEventToolbar group={group} event={evt} orgId={params.orgId} projectId={params.projectId} />
+            }
+            {group.status === 'muted' &&
+            <MutedBox statusDetails={group.statusDetails} />
+            }
+            {group.status === 'resolved' && group.statusDetails.inNextRelease &&
+            <div className="alert alert-info alert-block">
+              <span>{t(`This issue has been marked as being resolved in the next
+                          release. Until then, you will not get notified about new
+                          occurrences.`)}</span>
+            </div>
+            }
+            {group.status === 'resolved' && group.statusDetails.autoResolved &&
+            <div className="alert alert-info alert-block">
+              <span>{t(`This issue was automatically marked as resolved due to
+                          the Auto Resolve configuration for this project.`)}</span>
+            </div>
+            }
+            {this.state.loading ? <LoadingIndicator /> : (
+              this.state.error ? <LoadingError onRetry={this.fetchData} />
+              :
+              <EventEntries
+                group={group}
+                event={evt}
+                orgId={params.orgId}
+                projectId={params.projectId} />
+            )}
           </div>
-          }
-          {group.status === 'resolved' && group.statusDetails.autoResolved &&
-          <div className="alert alert-info alert-block">
-            <span>{t(`This issue was automatically marked as resolved due to
-                        the Auto Resolve configuration for this project.`)}</span>
-          </div>
-          }
-          {this.state.loading ? <LoadingIndicator /> : (
-            this.state.error ? <LoadingError onRetry={this.fetchData} />
-            :
-            <EventEntries
-              group={group}
-              event={evt}
-              orgId={params.orgId}
-              projectId={params.projectId} />
-          )}
+          <div className="col-md-3">
+            
           </div>
         </div>
       </div>
