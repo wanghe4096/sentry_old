240a595910a39acb4976bf71170ec2d22f1dae6d
wanghe <wanghe@loginsight.cn>
2016-01-11 11:40:54 +0800

    add field userkey for user table

--- src/sentry/api/serializers/models/host_stream.py
+++ src/sentry/api/serializers/models/host_stream.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from sentry.models.host_stream import Host, Stream, StreamType, HostType, Tag, LogEvent
 
@@ -6,7 +5,7 @@ from sentry.models.host_stream import Host, Stream, StreamType, HostType, Tag, L
 class HostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Host
-        fields = ('id', 'host_name', 'host_key', 'system', 'distver',  'host_type', 'user')
+        fields = ('id', 'host_name', 'host_key', 'system', 'distver',  'host_type', 'user', )
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -18,13 +17,13 @@ class TagSerializer(serializers.ModelSerializer):
 class StreamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stream
-        fields = ('id', 'stream_name',  'stream_type', 'host', 'user', 'tag')
+        fields = ('id', 'stream_name',  'stream_type', 'host', 'user', 'tag', )
 
 
 class HostTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = HostType
-        fields = ('id', 'host_type', 'user' )
+        fields = ('id', 'host_type', 'user', )
 
 
 class StreamTypeSerializer(serializers.ModelSerializer):
@@ -33,27 +32,12 @@ class StreamTypeSerializer(serializers.ModelSerializer):
         fields = ('id', 'stream_type', 'user',)
 
 
-# class EventSerializer(serializers.Serializer):
-#     payload = serializers.CharField(max_length=64*1024)
-#     offset = serializers.IntegerField()
-#
-#     def create(self, validated_data):
-#         return Event(**validated_data)
-
-
 class LogEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = LogEvent
         fields = ('id', 'payload', 'offset', 'stream', 'host', 'user',)
 
-#
-# class HostStreamSerializer(serializers.Serializer):
-#     host_types = HostTypeSerializer(required=False)
-#     host = HostSerializer(required=False)
-#     streams = StreamSerializer(required=False)
-#     tags = TagSerializer(required=False)
-#
-#
+
 
 
 
