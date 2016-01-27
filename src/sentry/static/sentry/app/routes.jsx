a3551941ee5ddcc6a2a7f085af0c603981768e19
kira <kira@kiradeMBP.lan>
2016-01-27 19:26:39 +0800

    新增SearchBoard

--- src/sentry/static/sentry/app/routes.jsx
+++ src/sentry/static/sentry/app/routes.jsx
@@ -34,7 +34,9 @@ import ReleaseNewEvents from 'views/releaseNewEvents';
 import RouteNotFound from 'views/routeNotFound';
 import SharedGroupDetails from 'views/sharedGroupDetails';
 import StorageIndex from 'views/storageIndex';
+import SearchIndex from 'views/searchIndex';
 import StorageApp from 'views/storageApp';
+import SearchApp from 'views/searchApp';
 import LogPreview from 'views/logPreview';
 import EventsIndex from 'views/eventsIndex';
 import Stream from 'views/stream';
@@ -42,84 +44,88 @@ import Stream from 'views/stream';
 import OrganizationStore from 'stores/organizationStore';
 
 function appendTrailingSlash(nextState, replaceState) {
-  let lastChar = nextState.location.pathname.slice(-1);
-  if (lastChar !== '/') {
-    replaceState(nextState, nextState.location.pathname + '/');
-  }
+    let lastChar = nextState.location.pathname.slice(-1);
+    if (lastChar !== '/') {
+        replaceState(nextState, nextState.location.pathname + '/');
+    }
 }
 
 const defaultOrg = OrganizationStore.items[0];
 
 let routes = (
-  <Route path="/" component={App}>
-    
-    <IndexRedirect to={defaultOrg.slug} />
+    <Route path="/" component={App}>
 
-    <Redirect from="/events" to={defaultOrg.slug+'/events'} />
-    <Redirect from="/storage" to={defaultOrg.slug+'/storage'} />
-    <Redirect from="/share/group/:shareId/" to="/share/issue/:shareId/" />
-    <Route path="/share/issue/:shareId/" component={SharedGroupDetails} />
+        <IndexRedirect to={defaultOrg.slug}/>
+
+        <Redirect from="/events" to={defaultOrg.slug+'/events'}/>
+        <Redirect from="/storage" to={defaultOrg.slug+'/storage'}/>
+        <Redirect from="/search" to={defaultOrg.slug+'/storage'}/>
+        <Redirect from="/share/group/:shareId/" to="/share/issue/:shareId/"/>
+        <Route path="/share/issue/:shareId/" component={SharedGroupDetails}/>
 
 
+        <Route path="/:orgId/" component={OrganizationDetails}>
 
-    <Route path="/:orgId/" component={OrganizationDetails}>
+            //<IndexRoute component={OrganizationTeams}/>
+            //<Route path="/organizations/:orgId/issues/assigned/" component={MyIssuesAssignedToMe}/>
+            //<Route path="/organizations/:orgId/issues/bookmarks/" component={MyIssuesBookmarked}/>
+            //<Route path="/organizations/:orgId/issues/history/" component={MyIssuesViewed}/>
+            <Route path="/organizations/:orgId/stats/" component={OrganizationStats}/>
+            //<Route path="/organizations/:orgId/rate-limits/" component={OrganizationRateLimits}/>
 
-      //<IndexRoute component={OrganizationTeams} />
-      //<Route path="/organizations/:orgId/issues/assigned/" component={MyIssuesAssignedToMe} />
-      //<Route path="/organizations/:orgId/issues/bookmarks/" component={MyIssuesBookmarked} />
-      //<Route path="/organizations/:orgId/issues/history/" component={MyIssuesViewed} />
-      <Route path="/organizations/:orgId/stats/" component={OrganizationStats} />
-      //<Route path="/organizations/:orgId/rate-limits/" component={OrganizationRateLimits} />
+            <IndexRedirect to="events"/>
 
-      <IndexRedirect to="events" />
-
-      <Route path="storage" component={StorageApp} >
-        <IndexRoute component={StorageIndex} />
-        <Route path="preview/:logId" component={LogPreview} />
-      </Route>
-
-      <Route path="events" component={EventsIndex}>
-        <IndexRoute component={OrganizationTeams} />
-        <Route path=":projectId/" component={ProjectDetails}>
-          <IndexRoute component={Stream} />
-
-          <Route path="dashboard/" component={ProjectDashboard} />
-          <Route path="releases/" component={ProjectReleases} />
-          <Route name="releaseDetails" path="releases/:version/" component={ReleaseDetails}>
-            <IndexRoute component={ReleaseNewEvents} />
-            <Route path="all-events/" component={ReleaseAllEvents} />
-            <Route path="artifacts/" component={ReleaseArtifacts} />
-          </Route>
-
-          <Route path="settings/" component={ProjectSettings}>
-            <Route path="install/" component={ProjectInstall}>
-              <IndexRoute component={ProjectInstallOverview}/>
-              <Route path=":platform/" component={ProjectInstallPlatform}/>
+            <Route path="search" component={SearchApp}>
+                <IndexRoute component={SearchIndex}/>
             </Route>
 
-            <Route path="*" component={RouteNotFound}/>
+            <Route path="storage" component={StorageApp}>
+                <IndexRoute component={StorageIndex}/>
+                <Route path="preview/:logId" component={LogPreview}/>
+            </Route>
 
-          </Route>
+            <Route path="events" component={EventsIndex}>
+                <IndexRoute component={OrganizationTeams}/>
+                <Route path=":projectId/" component={ProjectDetails}>
+                    <IndexRoute component={Stream}/>
 
-          <Redirect from="group/:groupId/" to="issues/:groupId/" />
+                    <Route path="dashboard/" component={ProjectDashboard}/>
+                    <Route path="releases/" component={ProjectReleases}/>
+                    <Route name="releaseDetails" path="releases/:version/" component={ReleaseDetails}>
+                        <IndexRoute component={ReleaseNewEvents}/>
+                        <Route path="all-events/" component={ReleaseAllEvents}/>
+                        <Route path="artifacts/" component={ReleaseArtifacts}/>
+                    </Route>
 
-          <Route path="issues/:groupId/" component={GroupDetails} ignoreScrollBehavior>
-            <IndexRoute component={GroupEventDetails} />
-            <Route path="activity/" component={GroupActivity} />
-            <Route path="events/:eventId/" component={GroupEventDetails} />
-            <Route path="events/" component={GroupEvents} />
-            <Route path="tags/" component={GroupTags} />
-            <Route path="tags/:tagKey/" component={GroupTagValues} />
-            <Route path="reports/" component={GroupUserReports} />
-          </Route>
-          <Route path="*" component={RouteNotFound}/>
+                    <Route path="settings/" component={ProjectSettings}>
+                        <Route path="install/" component={ProjectInstall}>
+                            <IndexRoute component={ProjectInstallOverview}/>
+                            <Route path=":platform/" component={ProjectInstallPlatform}/>
+                        </Route>
+
+                        <Route path="*" component={RouteNotFound}/>
+
+                    </Route>
+
+                    <Redirect from="group/:groupId/" to="issues/:groupId/"/>
+
+                    <Route path="issues/:groupId/" component={GroupDetails} ignoreScrollBehavior>
+                        <IndexRoute component={GroupEventDetails}/>
+                        <Route path="activity/" component={GroupActivity}/>
+                        <Route path="events/:eventId/" component={GroupEventDetails}/>
+                        <Route path="events/" component={GroupEvents}/>
+                        <Route path="tags/" component={GroupTags}/>
+                        <Route path="tags/:tagKey/" component={GroupTagValues}/>
+                        <Route path="reports/" component={GroupUserReports}/>
+                    </Route>
+                    <Route path="*" component={RouteNotFound}/>
+                </Route>
+
+            </Route>
         </Route>
 
-      </Route>
+        <Route path="*" component={RouteNotFound} onEnter={appendTrailingSlash}/>
     </Route>
-
-    <Route path="*" component={RouteNotFound} onEnter={appendTrailingSlash}/>
-  </Route>
 );
 
 export default routes;
