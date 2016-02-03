9a74b7fce70717a5bc29b362a16c10a268d13d2b
bold <bold@oluul.com>
2016-02-04 00:12:15 +0800

    update

--- src/sentry/static/sentry/app/components/home/homeHeader.jsx
+++ src/sentry/static/sentry/app/components/home/homeHeader.jsx
@@ -5,8 +5,16 @@ import ConfigStore from '../../stores/configStore';
 import HookStore from '../../stores/hookStore';
 import {t} from '../../locale';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ApiMixin from 'mixins/apiMixin';
+import AlertActions from 'actions/alertActions.jsx';
+import AlertStore from 'stores/alertStore';
 
 const HomeHeader = React.createClass({
+
+  mixins:[
+    ApiMixin
+  ],
+
   getInitialState: function () {
     return {show: true};
   },
@@ -16,6 +24,22 @@ const HomeHeader = React.createClass({
       show: !this.state.show
     });
     $(this.refs.header).toggleClass('ani-fold')
+  },
+
+  addDemo() {
+    AlertActions.addAlert(t('Being imported demo data!'), 'success', 1000 * 60 * 30);
+    const alertId = AlertStore.count;
+    this.api.request('/api/0/create_demo', {
+      method: 'post',
+      success: function (msg) {
+        AlertActions.addAlert(t('Create demo data is successfully!'), 'success', 3000);
+        AlertActions.closeAlert(alertId);
+      },
+      error: function () {
+        AlertActions.addAlert(t('Create demo data failed!'), 'error');
+        AlertActions.closeAlert(alertId);
+      }
+    });
   },
 
   render() {
@@ -35,13 +59,13 @@ const HomeHeader = React.createClass({
               <p>首次登陆需要创建一个组织用于管理团队和项目</p>
             </li>
             <li>
-              <a href="javasscript:;">
+              <a onClick={this.addDemo}>
                 <i className="fa fa-magic"/>加载Demo
               </a>
               <p>创建一个演示项目</p>
             </li>
             <li>
-              <a href="javascript:;">
+              <a href="http://www.loginsight.cn/contact_us.html" target="_blank">
                 <i className="fa fa-comments-o"/>联系我们
               </a>
               <p>可以把您的意见反馈给我们</p>
