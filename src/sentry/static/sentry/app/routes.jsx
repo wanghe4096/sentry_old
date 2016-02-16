0eeabcdea8b2fa420ba869fb0ffe6ddf57a393e2
bold <bold@oluul.com>
2016-02-16 23:29:33 +0800

    file cleanup

--- src/sentry/static/sentry/app/routes.jsx
+++ src/sentry/static/sentry/app/routes.jsx
@@ -34,10 +34,10 @@ import ReleaseNewEvents from 'views/releaseNewEvents';
 import RouteNotFound from 'views/routeNotFound';
 import SharedGroupDetails from 'views/sharedGroupDetails';
 import StorageIndex from 'views/storageIndex';
-import SearchIndex from 'views/searchIndex';
 import StorageApp from 'views/storageApp';
-import SearchApp from 'views/searchApp';
 import LogPreview from 'views/logPreview';
+import SearchIndex from 'views/searchIndex';
+import SearchApp from 'views/searchApp';
 import EventsIndex from 'views/eventsIndex';
 import Stream from 'views/stream';
 import ExtractApp from 'views/extract/extractApp';
@@ -57,35 +57,33 @@ function appendTrailingSlash(nextState, replaceState) {
 
 const defaultOrg = OrganizationStore.items[0];
 
-
 let routes = (
   <Route path="/" component={App}>
 
     <IndexRedirect to={defaultOrg.slug}/>
 
-    <Redirect from="/events" to={defaultOrg.slug+'/events'}/>
-    <Redirect from="/storage" to={defaultOrg.slug+'/storage'}/>
+    <Redirect from="/events" to={defaultOrg.slug + '/events'}/>
+    <Redirect from="/storage" to={defaultOrg.slug + '/storage'}/>
     <Redirect from="/search" to={defaultOrg.slug+'/search'}/>
-    <Redirect from="/home" to={defaultOrg.slug+'/home'} />
-    <Redirect from="/streamtype" to={defaultOrg.slug+'/streamtype'} />
+    <Redirect from="/home" to={defaultOrg.slug + '/home'}/>
+    <Redirect from="/streamtype" to={defaultOrg.slug + '/streamtype'}/>
 
     <Redirect from="/share/group/:shareId/" to="/share/issue/:shareId/"/>
     <Route path="/share/issue/:shareId/" component={SharedGroupDetails}/>
 
-
     <Route path="/:orgId/" component={OrganizationDetails}>
       {
         //<IndexRoute component={OrganizationTeams} />
-        //<Route path="/organizations/:orgId/issues/assigned/" component={MyIssuesAssignedToMe} />
-        //<Route path="/organizations/:orgId/issues/bookmarks/" component={MyIssuesBookmarked} />
-        //<Route path="/organizations/:orgId/issues/history/" component={MyIssuesViewed} />
-        //<Route path="/organizations/:orgId/rate-limits/" component={OrganizationRateLimits} />
-      }
+      //<Route path="/organizations/:orgId/issues/assigned/" component={MyIssuesAssignedToMe} />
+      //<Route path="/organizations/:orgId/issues/bookmarks/" component={MyIssuesBookmarked} />
+      //<Route path="/organizations/:orgId/issues/history/" component={MyIssuesViewed} />
+      //<Route path="/organizations/:orgId/rate-limits/" component={OrganizationRateLimits} />
+    }
       <Route path="/organizations/:orgId/stats/" component={OrganizationStats}/>
 
       <IndexRedirect to="home"/>
 
-      <Route path="home" component={HomeApp} />
+      <Route path="home" component={HomeApp}/>
 
       <Route path="storage" component={StorageApp}>
         <IndexRoute component={StorageIndex}/>
@@ -96,13 +94,10 @@ let routes = (
         <IndexRoute component={ExtractIndex}/>
         <Route path=":streamId/:action" component={ExtractorApp}>
           <IndexRoute component={ExtractorEvents}/>
-          {
-            //<Route path="role" component={ExtractorRole}/>
-          }
         </Route>
       </Route>
 
-      <Route path="kb/streamtype" component={StreamTypeIndex} />
+      <Route path="kb/streamtype" component={StreamTypeIndex}/>
 
       <Route path="search" component={SearchApp}>
         <IndexRoute component={SearchIndex}/>
--- src/sentry/static/sentry/app/views/app.jsx
+++ src/sentry/static/sentry/app/views/app.jsx
@@ -10,60 +10,54 @@ import BorderMenu from '../components/borderMenu';
 import {t} from '../locale';
 
 const App = React.createClass({
-    mixins: [
-        ApiMixin
-    ],
+  mixins: [ApiMixin],
 
-    getInitialState() {
-        return {
-            loading: false,
-            error: false
-        };
-    },
+  getInitialState() {
+    return {loading: false, error: false};
+  },
 
-    componentWillMount() {
-        this.api.request('/internal/health/', {
-            success: (data) => {
-                if (data && data.problems) {
-                    data.problems.forEach(problem => {
-                        AlertActions.addAlert(problem, 'error', 0);
-                    });
-                }
-            },
-            error: () => {
-            } // TODO: do something?
-        });
-
-        ConfigStore.get('messages').forEach((msg) => {
-            AlertActions.addAlert(msg.message, msg.level);
-        });
-    },
-
-    componentWillUnmount() {
-        // 暂时关闭,不清楚后遗症
-        //OrganizationStore.load([]);
-    },
-
-    render() {
-
-        let user = ConfigStore.get('user');
-
-        if (this.state.loading) {
-            return (
-                <LoadingIndicator triangle={true}>
-                    {t('Getting a list of all of your organizations.')}
-                </LoadingIndicator>
-            );
+  componentWillMount() {
+    this.api.request('/internal/health/', {
+      success: (data) => {
+        if (data && data.problems) {
+          data.problems.forEach(problem => {
+            AlertActions.addAlert(problem, 'error', 0);
+          });
         }
+      },
+      error: () => {} // TODO: do something?
+    });
 
-        return (
-            <div >
-                <Alerts className="messages-container"/>
-                <Indicators className="indicators-container"/>
-                {this.props.children}
-            </div>
-        );
+    ConfigStore.get('messages').forEach((msg) => {
+      AlertActions.addAlert(msg.message, msg.level);
+    });
+  },
+
+  componentWillUnmount() {
+    // 暂时关闭,不清楚后遗症
+    //OrganizationStore.load([]);
+  },
+
+  render() {
+
+    let user = ConfigStore.get('user');
+
+    if (this.state.loading) {
+      return (
+        <LoadingIndicator triangle={true}>
+          {t('Getting a list of all of your organizations.')}
+        </LoadingIndicator>
+      );
     }
+
+    return (
+      <div >
+        <Alerts className="messages-container"/>
+        <Indicators className="indicators-container"/>
+        {this.props.children}
+      </div>
+    );
+  }
 
 });
 
