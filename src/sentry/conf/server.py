269fd0d0d3a94af9a48d1bbd01671496378927e3
huhuayuan <584514950@qq.com>
2016-02-18 11:10:57 +0800

    server.py change chinese to english

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -936,7 +936,7 @@ SENTRY_API_RESPONSE_DELAY = 0
 # XXX(dcramer): this doesn't work outside of a source distribution as the
 # webpack.config.js is not part of Sentry's datafiles
 SENTRY_WATCHERS = (
-    # FIXED :$ sentry devserver 的时候无需启动webpack ，如需跑webpack 可用 webpack -w 来单独跑
+    # Disable start webpack for devserver, you can use "webpack -w" to start webpack
     # [os.path.join(NODE_MODULES_ROOT, '.bin', 'webpack'), '-d', '--watch',
     #  "--config={}".format(os.path.join(PROJECT_ROOT, os.pardir, os.pardir, "webpack.config.js"))],
 )
