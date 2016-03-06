38d999d40599d8856fb3de94a9b37d82a127dc62
wanghe <wanghe@loginsight.cn>
2016-03-06 14:09:18 +0800

    create index for tarantool backend

--- src/sentry/api/endpoints/indexes_index.py
+++ src/sentry/api/endpoints/indexes_index.py
@@ -10,8 +10,9 @@ from __future__ import absolute_import
 from sentry.api.base import Endpoint
 from sentry.models.log_indexes import Indexes
 from rest_framework.response import Response
+from django.conf import settings
 import datetime
-
+import requests
 
 class IndexesIndexEndpoint(Endpoint):
     permission_classes = []
@@ -35,6 +36,12 @@ class IndexesIndexEndpoint(Endpoint):
         data = request.DATA
         if len(data) == 0:
             return Response(status=400, data={'msg': 'no request parameters'})
+         url = "%s/tenant/test/%s/create?schema=key_format=r, value_format=QSQ," \
+                  " columns=(_id, user_id, movie_id, rating)" % (settings.SEARCH_SERVER_API, data['name'])
+        resp = requests.get(url)
+        if resp.status_code != 200:
+            return Response(resp.status_code, data={'msg': 'failed'})
+
         indexes = Indexes.objects.create(name=data['name'],
                                          created_at=datetime.datetime.now(),
                                          updated_at=datetime.datetime.now(),
@@ -42,6 +49,7 @@ class IndexesIndexEndpoint(Endpoint):
                                          dsn=data.get('dsn', None),
                                          desc=data.get('desc', None),
                                          user=request.user)
+
         if indexes:
             return Response(data, status=200)
         else:
