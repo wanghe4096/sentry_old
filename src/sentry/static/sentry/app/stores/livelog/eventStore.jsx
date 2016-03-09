e0c8280efe20898ed7f6bdd719cc9de3e24a02a2
bold <bold@oluul.com>
2016-03-09 22:18:11 +0800

    add commont

--- src/sentry/static/sentry/app/stores/livelog/eventStore.jsx
+++ src/sentry/static/sentry/app/stores/livelog/eventStore.jsx
@@ -4,6 +4,7 @@ import EventAction from 'actions/livelog/eventAction';
 const EventStore = Reflux.createStore({
   listenables: EventAction,
   onSend(event) {
+    // TODO 需要做节流阀 + 队列，防止前端压力过大
     this.trigger(event)
   }
 });
