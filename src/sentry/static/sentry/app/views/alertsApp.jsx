1efdb8ecf43d3ef62fbcd200c140b01a777146d1
huhuayuan <584514950@qq.com>
2016-03-10 19:09:07 +0800

    add alert content

--- src/sentry/static/sentry/app/views/alertsApp.jsx
+++ src/sentry/static/sentry/app/views/alertsApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Reflux from 'reflux';
 import DocumentTitle from 'react-document-title';
-import {Modal,Button,Input} from 'react-bootstrap';
+import {Modal,Button,Input,MenuItem,DropdownButton} from 'react-bootstrap';
 import {t} from 'app/locale';
 
 const style = require('css/alerts.less');
@@ -47,16 +47,131 @@ const AlertsApp = React.createClass({
                         type="text"
                         name="alertname"
                         value={this.state.hostName}
-                        placeholder={t('输入您的警告名称 ...')}
+                        placeholder={t('输入您的告警名称 ...')}
                         label={t('名称')}
-                        help={t('* 设置您的警告名,方便搜索.')}
+                        help={t('* 设置您的告警名称,方便搜索.')}
                         //hasFeedback
                         autoFocus
                         //disabled={inSaving}
-                        labelClassName=""
-                        wrapperClassName=""
+                        labelClassName="col-xs-2"
+                        wrapperClassName="col-xs-10"
+                        onChange={this.handleChange}
+                        className="alert-main"
+                      />
+                      <Input
+                        type="text"
+                        name="alertdesc"
+                        value={this.state.hostName}
+                        placeholder={t('输入您的告警描述 ...')}
+                        label={t('描述')}
+                        help={t('* 设置您的告警描述,方便查询.')}
+                        //hasFeedback
+                        autoFocus
+                        //disabled={inSaving}
+                        labelClassName="col-xs-2"
+                        wrapperClassName="col-xs-10"
                         onChange={this.handleChange}
                       />
+                      <div className="form-group clearfix">
+                        <label className="col-xs-2">{t('搜索内容')}</label>
+                        <span className="col-xs-10">
+                        {t('此处为从search获取的搜索语句')}
+                        </span>
+                      </div>
+                      <Input
+                        type="select"
+                        label={t('告警类型')}
+                        labelClassName="col-xs-2"
+                        wrapperClassName="col-xs-10"
+                        placeholder={t('选择您的告警类型 ...')}
+                        help={t('* 选择您需要的告警类型.')}
+                        className="select-height">
+                        <option value="select">{t('Any')}</option>
+                        <option value="other">{t('BlackList')}</option>
+                        <option value="other">{t('Whitelist')}</option>
+                        <option value="other">{t('Change')}</option>
+                        <option value="other">{t('Frequency')}</option>
+                        <option value="other">{t('Spike')}</option>
+                        <option value="other">{t('Flatline')}</option>
+                        <option value="other">{t('New Term')}</option>
+                        <option value="other">{t('Cardinality')}</option>
+                      </Input>
+                      <div className="form-group clearfix">
+                        <label className="col-xs-2">{t('告警方式')}</label>
+                        <div className="col-xs-10">
+                          <Input
+                            type="checkbox"
+                            label={t('Email')}
+                            labelClassName=""
+                            checked>
+                          </Input>
+                          <Input
+                            type="text"
+                            name="alertdesc"
+                            value={this.state.hostName}
+                            placeholder={t('输入您的告警邮箱 ...')}
+                            help={t('* 系统会将告警信息发送至您设置的邮箱.')}
+                            //hasFeedback
+                            autoFocus
+                            //disabled={inSaving}
+                            wrapperClassName="col-xs-12 no-p-l-r"
+                            onChange={this.handleChange}
+                          />
+                        <span className="alert-option">
+                          <i className="fa fa-caret-right"></i>
+                          {t('Other Options')}
+                        </span>
+                        <ul className="other-option">
+                          <li>
+                            <label className="fa fa-weixin"></label>
+                            <div className="col-md-12">
+                              <Input
+                                type="checkbox"
+                                label={t('Web hook')}
+                                labelClassName=""
+                                checked>
+                              </Input>
+                              <Input
+                                type="text"
+                                name="alertdesc"
+                                value={this.state.hostName}
+                                placeholder={t('输入您的告警邮箱 ...')}
+                                help={t('* 系统会将告警信息发送至Web hook.')}
+                                //hasFeedback
+                                autoFocus
+                                //disabled={inSaving}
+                                wrapperClassName="col-xs-12 no-p-l-r"
+                                onChange={this.handleChange}
+                              />
+                            </div>
+                          </li>
+                          <li>
+                            <label className="fa fa-weixin"></label>
+                            <div className="col-md-12">
+                              <Input
+                                type="checkbox"
+                                label={t('one alert')}
+                                labelClassName=""
+                                checked>
+                              </Input>
+                              <Input
+                                type="text"
+                                name="alertdesc"
+                                value={this.state.hostName}
+                                placeholder={t('输入您的服务账号 ...')}
+                                help={t('* 系统会将告警信息发送至您的onealert账号.')}
+                                //hasFeedback
+                                autoFocus
+                                //disabled={inSaving}
+                                wrapperClassName="col-xs-12 no-p-l-r"
+                                onChange={this.handleChange}
+                              />
+                            </div>
+                          </li>
+                        </ul>
+                        </div>
+                      </div>
+
                   </div>
                 </div>
                 <div className="col-md-3"></div>
